0b48d2d1044a2e75c7134272ae8059ebbc94c94e
hdavider <hdavider@gmail.com>
2023-10-22 17:17:26 -0600

    Terminado

--- servidor.js
+++ servidor.js
@@ -2,10 +2,21 @@ let http = require('http');
 let host = '127.0.0.1';
 let puerto = '9000';
 
-let servidor = http.createServer(function(llamado, respuesta) {
-    respuesta.writeHead(200, {'Content-Type' : 'text/html'});
-    respuesta.end('<h1>Hola Mundo!</h1>');
-})
+const nombre = 'Hugo Erazo';
+const areasConocimiento = ['Programacion', 'Soporte Tecnico'];
+
+let servidor = http.createServer(function (llamado, respuesta) {
+  respuesta.writeHead(200, { 'Content-Type': 'text/html' });
+  respuesta.write('<h1>Hola Mundo otra vez!</h1>');
+  respuesta.write(`<p>Mi nombre es ${nombre}</p>`);
+  respuesta.write('<p>Areas de conocimiento:</p>');
+  respuesta.write('<ul>');
+  areasConocimiento.forEach((area) => {
+    respuesta.write(`<li>${area}</li>`);
+  });
+  respuesta.write('</ul>');
+  respuesta.end();
+});
 
 servidor.listen(puerto, host, function(){
     console.log('El servidor esta corriendo: ' + host + ":" + puerto)
